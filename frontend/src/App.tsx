1bbea08ec9f0be84c25a54f2e7183533c5395ce8
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-07 15:01:41 +0100

    feat: ajout de premiers composants pour la page de connexion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
 import TextSection from './TextSection.tsx'
 import LoginSection from './LoginSection.tsx'
+import Header from './Header.tsx'
 
 
 function App() {
   return (
     <>
-      <TextSection 
+    <div className="h-screen w-full bg-gray-500 text-white flex flex-col items-center justify-center">
+      <Header 
         title={"ChessWar"}
+      />
+
+      <TextSection 
+        title={"Bienvenue"}
         paragraph={"Ceci est la landing page où il y aura un long texte pour remplir le paragraphe. Je ne sis pas quoi dire mais je dis des choses utile ou inutiles. Nous allons faire un jeu de chess pour que le gens se la foutent mdr. J'ai pensé à un nom: ChessWar la guerre des rois ; pas mal hein? Anyway, je pense que j'ai assez ecris, bye !"}
       />
       <TextSection 
@@ -15,6 +21,7 @@ function App() {
         paragraph={"Connectez-vous vite et découvrez l'art de la GUERRE !"}
       />
       <LoginSection />
+    </div>
     </>
   )
 }
