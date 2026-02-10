1abc636f2f7127b6ee0a53922e3714cb039c66c8
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-11 00:09:44 +0100

    fix: moved postgres-node driver instance and dotenv.config() from index.ts to main.ts for easier access

--- backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import { Pool } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-dotenv.config();
-
-export const pool = new Pool({ connectionString: process.env.POSTGRES_URL! });
-export const db = drizzle({ client: pool });
