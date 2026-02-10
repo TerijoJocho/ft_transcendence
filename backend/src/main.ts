def6f8edb8da6d0be72798e58e987df1fcdc7dbb
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-11 00:09:44 +0100

    fix: moved postgres-node driver instance and dotenv.config() from index.ts to main.ts for easier access

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
+dotenv.config();
+const pool = new Pool({ connectionString: process.env.POSTGRES_URL! });
+export const db = drizzle({ client: pool });
+  
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
