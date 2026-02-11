f17f6897a10435bf1a4dadef5a709403f1942bcc
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-11 17:43:11 +0100

    fix: moved import and dotenv.config at the top of file to force env var import before any other application process (and allow postgres-node driver instanciation)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
-
-dotenv.config();
   
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
