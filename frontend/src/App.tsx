be7476832596dd1f424c0e746813b18e7d2245b4
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-11 11:52:06 +0100

    feat(auth): setup auth flow with routing and UI: add Login, Signin and Dashboard pages, configure React Router navigation, create api.ts abstraction for fetch calls, add basic route protection logic, setup Tailwind styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,33 +23,41 @@ function App() {
   // Les dépendances assurent que cet effet s'exécute si login/clearAuth changent
 
   return (
-    <Routes>
-      {/* Redirection par default */}
-      <Route path="/" element={<Navigate to="/login" replace />} />
+    <div
+      className="
+        min-h-screen
+        bg-[linear-gradient(90deg,#121216_0%,#121216_7.692%,#16171a_calc(7.692%+1px),#16171a_15.385%,#1a1b1f_calc(15.385%+1px),#1a1b1f_23.077%,#1f2023_calc(23.077%+1px),#1f2023_30.769%,#232427_calc(30.769%+1px),#232427_38.462%,#27282b_calc(38.462%+1px),#27282b_46.154%,#2b2c2f_calc(46.154%+1px),#2b2c2f_53.846%,#2f3033_calc(53.846%+1px),#2f3033_61.538%,#323337_calc(61.538%+1px),#323337_69.231%,#36373a_calc(69.231%+1px),#36373a_76.923%,#38393d_calc(76.923%+1px),#38393d_84.615%,#3b3c40_calc(84.615%+1px),#3b3c40_92.308%,#3d3e42_calc(92.308%+1px)_100%)]
+        flex justify-center items-center
+        "
+    >
+      <Routes>
+        {/* Redirection par default */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
 
-      <Route path="/signup" element={<SignIn />} />
+        <Route path="/signup" element={<SignIn />} />
 
-      <Route
-        path="/login"
-        element={
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        }
-      />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
 
-      <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
 
-      {/* Fallback si route inconnue */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+        {/* Fallback si route inconnue */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </div>
   );
 }
 
