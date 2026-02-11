00f8eebfb0d185a6f9fd0c3e4e74aa9885c771d4
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-11 19:50:28 +0100

    style: removed useless comment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,11 @@ dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-  
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-// Au lieu de juste bootstrap();
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
   // Si le serveur ne démarre pas, on tue le processus pour que Docker le redémarre
