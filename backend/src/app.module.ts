ccfcda3eb0d014a302b305142b43c39bcafa1dd5
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-15 00:05:48 +0100

    feat: added shared module to AppModule class decorator (imports)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SigninModule } from './signin/signin.module';
+import { SharedModule } from './shared/shared.module';
 
-@Module({
-  imports: [SigninModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+@Module({ 
+imports: [SharedModule, SigninModule], 
+controllers: [AppController], 
+providers: [AppService], 
+}) export class AppModule {}
