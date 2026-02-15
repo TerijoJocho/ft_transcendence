11637a536b2c62d033c0c0bd7f13a4bab9f43f25
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-15 18:34:43 +0100

    style/fix: turning signin feature from module to service, provider for authentication module. Fixed relative import paths. Completeed signin service test. Imported and constructed UtilsService into SigninService

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { SigninService } from './signin/signin.service';
+
+@Module({
+  imports: [],
+  controllers: [AuthController],
+  providers: [SigninService],
+})
+export class AuthModule {}
