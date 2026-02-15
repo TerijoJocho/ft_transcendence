5d9c296dd85a3e28f37e8151f0d9cc9706696908
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-15 18:41:55 +0100

    style: removed logic from main. DTO format validation pipe will be a globally availalable and enabled provider of sharedModule. CORS configs can be directly implemented in the nestFactory.create() func as parameter.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,27 +1,18 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: '.env' });
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // Retire tout champ qui n'est pas dans le DTO (Sécurité++)
-      forbidNonWhitelisted: true, // Renvoie une erreur si un champ inconnu est envoyé
-    }),
+  const app = await NestFactory.create(AppModule, 
+    { cors: { 
+        origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // frontend
+        credentials: true, // IMPORTANT pour cookies
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type'],} 
+    }
   );
-
-  app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // frontend
-    credentials: true, // IMPORTANT pour cookies
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
-  });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
