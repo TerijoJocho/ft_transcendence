e0cce016e47ee65bfc4eb7105eb5aad679b43742
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-17 00:07:46 +0100

    feat(init): add two buttons on game-page, add one little thing in index.css and a anti overflowing class in PrivateLayout

--- frontend/src/PrivateLayout.tsx
+++ frontend/src/PrivateLayout.tsx
@@ -3,9 +3,9 @@ import SideBar from "./components/SideBar.js";
 
 export default function PrivateLayout() {
     return (
-        <div className="flex min-h-screen">
+        <div className="flex min-h-screen min-w-screen">
             <SideBar />
-            <main className="flex-1 p-6">
+            <main className="flex-1 p-6 overflow-auto">
                 <Outlet />
             </main>
         </div>
--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -2,10 +2,19 @@ import Header from "../components/Header.tsx";
 
 function Game() {
     return (
-        <div className="text-white border min-w-max">
+        <div className="text-white border">
             <Header 
                 title="Démarrez une partie !"
             />
+
+            <section className="flex justify-evenly">
+                <button className="p-4 bg-white rounded-md text-black hover:bg-slate-300">
+                    Joueur vs IA
+                </button>
+                <button className="p-4 bg-white rounded-md text-black hover:bg-slate-300">
+                    Joueur vs Joueur
+                </button>
+            </section>
         </div>
     );
 }
