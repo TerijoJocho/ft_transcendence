6b8d125530127b898b3ea5a6c68f768fbf5a6b8c
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-02-18 17:07:42 +0100

    refractoring: rien de special

--- frontend/src/PublicLayout.tsx
+++ frontend/src/PublicLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function PublicLayout() {
     return (
-        <div className="min-h-screen flex justify-center items-center m-auto">
+        <div className="min-h-screen flex items-center m-auto">
             <main className="flex-1">
                 <Outlet />
             </main>
