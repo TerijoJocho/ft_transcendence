151f3c2974224e4d8418afcf213863da0bbb0308
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-02-18 17:32:12 +0100

    fix: moved redis client instanciation into onModuleInit

--- backend/src/shared/services/redis.service.ts
+++ backend/src/shared/services/redis.service.ts
@@ -4,9 +4,10 @@ import { OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 
 @Injectable()
 export class RedisService implements OnModuleInit, OnModuleDestroy {
-  private redis = createClient({ url: process.env.REDIS_URL! });
+  private redis: ReturnType<typeof createClient>; 
   
   async onModuleInit() {
+    this.redis = createClient({ url: process.env.REDIS_URL! });
     await this.redis.connect();
   }
   onModuleDestroy() {
