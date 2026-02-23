e8836764089675b776d0412e2113334adae15deb
500 <kalvinsam31@gmail.com>
2026-02-23 16:35:45 +0100

    fixe lint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,14 +4,14 @@ import { SigninDto } from './signin/signin.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private signinService: SigninService) { }
+  constructor(private signinService: SigninService) {}
 
   @Post('register')
   register(@Body() bodyDto: SigninDto) {
     return this.signinService.registerPlayers(
       bodyDto.pseudo,
       bodyDto.mail,
-      bodyDto.password
+      bodyDto.password,
     );
   }
 }
--- backend/src/auth/login/login.service.ts
+++ backend/src/auth/login/login.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LoginService {
-
-    
-}
+export class LoginService {}
