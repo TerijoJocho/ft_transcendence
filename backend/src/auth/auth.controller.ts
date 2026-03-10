bb904f63d6259d1273f1aefd662b8d14bc17d76a
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-03-10 23:02:46 +0100

    feat: added login, refresh and logout post routes with befitting guards and functions

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,45 @@
-import { Body, Controller, HttpCode, HttpStatus, NotImplementedException, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Controller, Get, HttpCode, HttpStatus, Post, Request, Res, UseGuards } from '@nestjs/common';
+import { PassportLocalGuard } from './guards/passport-local.guard';
+import { PassportJwtGuard } from './guards/passport-jwt.guard';
+import type { Response } from 'express';
+import { CurrentUser } from './decorator/current-user.decorator';
+import { responseLoginDto } from './dto/response-login.dto';
+import { logoutDto } from './dto/logout.dto';
+import { PassportJwtRefreshGuard } from './guards/passport-jwt-refresh.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-  
+  constructor(private authService: AuthService) {} 
+
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  login(@Body() input: { username: string; password: string }) {
-    return this.authService.authenticate(input);
+  @UseGuards(PassportLocalGuard)
+  async login(
+    @CurrentUser() user: responseLoginDto,
+    @Res({ passthrough: true }) response: Response) {
+	    await this.authService.login(user, response);
+  }
+
+  @Post('refresh')
+  @UseGuards(PassportJwtRefreshGuard)
+  async refresh(
+    @CurrentUser() user: responseLoginDto,
+    @Res({ passthrough: true }) response: Response) {
+      await this.authService.renewAccessToken(user, response);
+  }
+
+  @Post('logout')
+  @UseGuards(PassportJwtGuard)
+  async logout(
+    @CurrentUser() user: logoutDto,
+    @Res({ passthrough: true }) response: Response) {
+      await this.authService.logOut(user, response);
+  }
+  
+  @UseGuards(PassportJwtGuard)
+  @Get('me')
+  async getUserInfo(@Request() request) {
+	  return request.user;
   }
 }
