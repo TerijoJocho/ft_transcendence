b3cc3592f57ed0f373740700be5ae70d1eba9472
500 <kalvinsam31@gmail.com>
2026-03-11 21:32:37 +0100

    feat: register CRUD, mise en place de la route delete, NON TESTER

--- backend/src/auth/userService/user.service.ts
+++ backend/src/auth/userService/user.service.ts
@@ -1,18 +1,27 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import {Injectable, Param, Post, Get } from '@nestjs/common';
 import { UtilsService } from 'src/shared/services/utils.func.service';
+import { eq } from 'drizzle-orm';
+import { playerTable } from 'src/shared/db/schema';
 
-@injectable()
+@Injectable()
 export class UserService{
     constructor(private readonly utils: UtilsService) {}
 
-    @Post()
-    deleteUser(userId: number) {
-        return this.utils.deletePlayers(userId);
+    deleteUserbyId(playerId: number) {
+        return this.utils.deletePlayersBy(
+            'and',
+            { 
+                playerId: playerTable.playerId,
+                gameName: playerTable.gameName,
+                mailAddress: playerTable.mailAddress,
+            },
+            eq(playerTable.playerId, playerId),
+        );
+        //supp le token, ou le cookie
     }
 
-    @Post()
-    logoutUser(@Param(':id') userId: number) {
-        //supp le token, ou le cookie 
+    logoutUser(userId: number) {
+        
         return { message: 'User logged out successfully' };
     }
 }
