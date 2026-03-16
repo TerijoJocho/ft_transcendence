165e573c3ef23371afe8fbc03f71ab7155e35f91
500 <kalvinsam31@gmail.com>
2026-03-16 17:40:18 +0100

    feat - vault:  mise en place du service vault (dockerfile, data, policy, script d init)

--- backend/vault/init.sh
+++ backend/vault/init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 vault server -config=/vault/config/vault-config.hcl &
 
 VAULT_PID=$!
@@ -8,31 +7,84 @@ echo "⏳ Attente de 5 secondes..."
 sleep 5
 
 export VAULT_CACERT=/etc/vault/certs/vault-selfsigned.crt
-export VAULT_ADDR='https://127.0.0.1:8200'
+export VAULT_ADDR='https://vault:8200'
 
-vault operator init -format=json > /tmp/vault_init.json
+status=$(vault status -format=json || true)
+initialized=$(echo "$status" | jq -r '.initialized')
+sealed=$(echo "$status" | jq -r '.sealed')
 
-export VAULT_ROOT_TOKEN=$(jq -r '.root_token // empty' /tmp/vault_init.json)
-export VAULT_UNSEAL_KEY_1=$(jq -r '.unseal_keys_hex[0] // empty' /tmp/vault_init.json)
-export VAULT_UNSEAL_KEY_2=$(jq -r '.unseal_keys_hex[4] // empty' /tmp/vault_init.json)
-export VAULT_UNSEAL_KEY_3=$(jq -r '.unseal_keys_hex[2] // empty' /tmp/vault_init.json)
+if [ "$initialized" != "true" ]; then
+  echo "Initializing Vault..."
+  vault operator init -key-shares=5 -key-threshold=3 -format=json > /tmp/vault_init.json
 
-vault operator unseal "$VAULT_UNSEAL_KEY_1"
-vault operator unseal "$VAULT_UNSEAL_KEY_2"
-vault operator unseal "$VAULT_UNSEAL_KEY_3"
+  export VAULT_TOKEN=$(jq -r '.root_token // empty' /tmp/vault_init.json)
+  echo $VAULT_TOKEN > /run/secrets/root_token
+  export VAULT_UNSEAL_KEY_1=$(jq -r '.unseal_keys_hex[0] // empty' /tmp/vault_init.json)
+  export VAULT_UNSEAL_KEY_2=$(jq -r '.unseal_keys_hex[4] // empty' /tmp/vault_init.json)
+  export VAULT_UNSEAL_KEY_3=$(jq -r '.unseal_keys_hex[2] // empty' /tmp/vault_init.json)
 
-vault login "$VAULT_ROOT_TOKEN"
+  if [ -n "$VAULT_UNSEAL_KEY_1" ] && [ -n "$VAULT_UNSEAL_KEY_2" ] && [ -n "$VAULT_UNSEAL_KEY_3" ]; then
+  echo "$VAULT_UNSEAL_KEY_1" > /run/secrets/unseal_keys
+  echo "$VAULT_UNSEAL_KEY_2" >> /run/secrets/unseal_keys
+  echo "$VAULT_UNSEAL_KEY_3" >> /run/secrets/unseal_keys
+  else
+  echo "Error: Missing unseal keys, cannot store them in /run/secrets/unseal_keys"
+  exit 1
+  fi
 
-vault policy write backend-policy /backend-policy.hcl
+  vault operator unseal "$VAULT_UNSEAL_KEY_1"
+  vault operator unseal "$VAULT_UNSEAL_KEY_2"
+  vault operator unseal "$VAULT_UNSEAL_KEY_3"
 
-vault enable -path=secret kv-v2
+  vault login "$VAULT_TOKEN"
+  vault policy write backend-policy /backend-policy.hcl
+  if ! vault secrets list -format=json | jq -e 'has("secret/")' >/dev/null; then
+    vault secrets enable -version=2 -path=secret kv
+  fi
+  vault kv put secret/db password=$POSTGRES_PASSWORD username=$POSTGRES_USER
 
-vault kv put secret/db password=$POSTGRES_PASSWORD username=$POSTGRES_USER
+  vault token create -policy="backend-policy" -format=json > /tmp/vault_backend_token.json
+  export BACKEND_TOKEN=$(jq -r '.auth.client_token // empty' /tmp/vault_backend_token.json)
+  echo $BACKEND_TOKEN > /run/secrets/backend_token
 
-vault token create -policy="backend-policy" -format=json > /tmp/vault_backend_token.json
+else
 
-export BACKEND_TOKEN=$(jq -r '.auth.client_token // empty' /tmp/vault_backend_token.json)
+  if [ "$sealed" = "true" ]; then
+    echo "Attempting to unseal using stored keys..."
 
-echo $BACKEND_TOKEN > /vault/secrets/backend_token
+    if [ -f /run/secrets/unseal_keys ]; then
+      for key in $(cat /run/secrets/unseal_keys); do
+        vault operator unseal "$key"
+      done
+    else
+      echo "No unseal keys available, manual unseal required."
+      exit 1
+    fi
+  fi
+
+  if [ -f /run/secrets/root_token ]; then
+    ROOT_TOKEN=$(cat /run/secrets/root_token)
+  fi
+  if [ -n "$ROOT_TOKEN" ] && VAULT_TOKEN="$ROOT_TOKEN" vault token lookup >/dev/null 2>&1; then
+    echo "valid token found in init file, using it"
+    vault login "$ROOT_TOKEN"
+  else
+    echo "token in init file is invalid, checking for token in /run/secrets/backend_token..."
+    exit 1
+  fi
+
+  if [ -f /run/secrets/backend_token ]; then
+    BACKEND_TOKEN="$(cat /run/secrets/backend_token)"
+  fi
+
+  if VAULT_TOKEN="$BACKEND_TOKEN" vault token lookup >/dev/null 2>&1; then
+    echo "valid token found"
+  else
+    echo "invalid token, creating new one"
+    vault token create -policy="backend-policy" -format=json > /tmp/vault_backend_token.json
+    BACKEND_TOKEN=$(jq -r '.auth.client_token // empty' /tmp/vault_backend_token.json)
+    echo $BACKEND_TOKEN > /run/secrets/backend_token
+  fi
+fi
 
 wait $VAULT_PID
