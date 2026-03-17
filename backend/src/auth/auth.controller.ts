a71c2892bc257ac73b2a87a3e4853ccf372c8aad
500 <kalvinsam31@gmail.com>
2026-03-17 20:46:34 +0100

    feat:user - mise en place du service user fonction delete + update a finaliser et tester

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,11 @@
-import { Body, Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SigninService } from './signin/signin.service';
 import { SigninDto } from './signin/signin.dto';
-import { UserService } from './userService/user.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private signinService: SigninService,
-    private userService: UserService,
   ) {}
 
   @Post('register')
@@ -17,21 +15,5 @@ export class AuthController {
       bodyDto.pseudo,
       bodyDto.password,
     );
-  }
-
-  @Post('auth/me')
-  authMe() {
-    return { message: 'Authenticated user data would be here' };
-  }
-
-  @Post('delete/:id') // penser a verifier que c'est bien le user qui veut supprimer son compte
-  async deleteUser(@Param('id', ParseIntPipe) playerId: number) {
-    await this.userService.deleteUserbyId(playerId);
-    return { message: 'User deleted successfully' };
-  }
-
-  @Post('update')
-  update() {
-    return { message: 'User data updated successfully' };
   }
 }
