671ef33bcbcdcd798568a6b4875d3cfbf109dacd
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-03-24 17:49:55 +0100

    fix: ajout de la désactivation de l'ESLint pour setState dans useEffect dans le composant AddFriends

--- frontend/src/components/AddFriends.tsx
+++ frontend/src/components/AddFriends.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useState, useEffect } from "react";
 import Search from "./Search.tsx";
 import { searchUser, addFriend } from "../api/api.ts";
