0317f29a6fa776927d2f36057e7e8182a8995a1e
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-03-25 18:34:42 +0100

    feat: game related module, controller and services managing game creationdeletion, status and result updating for both players

---
+++ backend/src/game/game.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Post, UseGuards, Body, ParseIntPipe, Param } from '@nestjs/common';
+import { PassportJwtGuard } from 'src/auth/guards/passport-jwt.guard';
+import { NewGameDto } from './dto/new-game.dto';
+import { EndGameDto } from './dto/end-game.dto';
+import { GameService } from './game.service';
+import { LogoutDto } from 'src/auth/dto/logout.dto';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+
+@Controller('game')
+export class GameController {
+  constructor(private readonly GameService: GameService) {}
+
+  @Post('create')
+  @UseGuards(PassportJwtGuard)
+  create( 
+	@CurrentUser() user: LogoutDto,
+	@Body() bodyDto: NewGameDto) {
+	return this.GameService.generateNewGame(bodyDto, user.playerId);
+  }
+
+  @Post(':gameId/join')
+  @UseGuards(PassportJwtGuard)
+  join(
+	@CurrentUser() user: LogoutDto,
+	@Param('gameId', ParseIntPipe) gameId: number) {
+	return this.GameService.joinGame(gameId, user.playerId);
+  }
+
+  @Post(':gameId/end')
+  @UseGuards(PassportJwtGuard)
+  end(
+	@CurrentUser() user: LogoutDto,
+	@Param('gameId', ParseIntPipe) gameId: number,
+	@Body() bodyDto: EndGameDto) {
+	return this.GameService.endGame(bodyDto, gameId, user.playerId);
+  }
+
+  @Post(':gameId/cancel')
+  @UseGuards(PassportJwtGuard)
+  cancel(
+	@CurrentUser() user: LogoutDto,
+	@Param('gameId', ParseIntPipe) gameId: number) {
+	return this.GameService.cancelGame(gameId, user.playerId);
+  }
+}
