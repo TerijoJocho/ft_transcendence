80411ea3f6e6dad2b67450f4304735d5192fc9d4
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-03-25 18:34:42 +0100

    feat: game related module, controller and services managing game creationdeletion, status and result updating for both players

---
+++ backend/src/game/dto/new-game.dto.ts
@@ -0,0 +1,10 @@
+import { IsIn, IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+
+export class NewGameDto {
+	@IsNotEmpty()
+	@IsIn(['BLACK', 'WHITE'])
+	readonly playerColor: string;
+
+	@IsIn(['CLASSIC', 'BLITZ', 'BULLET'])
+	readonly gameMode?: string;
+}
