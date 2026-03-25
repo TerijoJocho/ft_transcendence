089823e7fc560c5f7455e8b889db2c75ac8da7e5
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-03-25 18:30:49 +0100

    feat: added game module to app module imports list

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import { SigninModule } from './signin/signin.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [SharedModule, AuthModule],
+  imports: [SharedModule, AuthModule, SigninModule, GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
