562d2af6ad2b691fa7bcd273e1bf8826e673047d
500 <kalvinsam31@gmail.com>
2026-03-30 14:13:43 +0200

    feat(user): lint modifs

--- backend/src/users/updateDto.ts
+++ backend/src/users/updateDto.ts
@@ -1,19 +1,18 @@
-import  { IsString, IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
 
-export class UpdateUserDto 
-{
-    @IsNotEmpty()
-    @IsString()
-    @IsOptional()
-    gameName?: string;
+export class UpdateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  gameName?: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    @IsOptional()
-    mailAddress?: string;
+  @IsNotEmpty()
+  @IsEmail()
+  @IsOptional()
+  mailAddress?: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @IsOptional()
-    password?: string;
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  password?: string;
 }
