bcc17bb67db1b54ac933574af0f4f44a4108874c
500 <kalvinsam31@gmail.com>
2026-04-07 18:31:15 +0200

    feat(users): add users module and DTOs

---
+++ backend/src/users/dto/updateDto.ts
@@ -0,0 +1,34 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  IsOptional,
+  MaxLength,
+  IsUrl,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  gameName?: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @IsOptional()
+  mailAddress?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  password?: string;
+
+  @IsString()
+  @MaxLength(2048)
+  @IsOptional()
+  @IsUrl(
+    { require_protocol: true, protocols: ['https'] },
+    { message: 'avatarUrl must be a valid https URL' },
+  )
+  avatarUrl?: string;
+}
