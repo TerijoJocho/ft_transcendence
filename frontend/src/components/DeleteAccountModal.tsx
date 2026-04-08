4cdd2228d2f46312144602640778a7532175c5fd
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-04-08 19:15:25 +0200

    feat: integrate PrimeReact and enhance user profile management
    - Added PrimeReact library for UI components.
    - Updated user profile management functions in api.ts to include user stats and profile updates.
    - Refactored DeleteAccountModal component to handle account deletion with confirmation.
    - Improved HeaderPlayerInfos and SideBar components to utilize new avatar URL structure.
    - Enhanced Profil page to include double authentication option and improved feedback messages.
    - Updated isValidMail utility to allow for hyphens in email addresses.
    - Cleaned up unused code and comments across various components and data files.

--- frontend/src/components/DeleteAccountModal.tsx
+++ frontend/src/components/DeleteAccountModal.tsx
@@ -1,10 +1,71 @@
-export default function DeleteAccountModal({setWantToDelete}) {
+import type { Dispatch, SetStateAction } from "react";
 
-    return(
-        <section className="self-end">
-            <button className="m-2 bg-red-600 text-white warning-hover hover:bg-white" onClick={() => setWantToDelete(true)}>
-                Supprimer le compte
-            </button>
-        </section>
-    )
+type DeleteAccountModalProps = {
+  setWantToDelete: Dispatch<SetStateAction<boolean>>;
+  deleteInput: string;
+  setDeleteInput: Dispatch<SetStateAction<string>>;
+  passwordInput: string;
+  setPasswordInput: Dispatch<SetStateAction<string>>;
+  CONFIRM_PHRASE: string;
+  handleDelete: () => void;
+};
+
+export default function DeleteAccountModal({
+  setWantToDelete,
+  deleteInput,
+  setDeleteInput,
+  passwordInput,
+  setPasswordInput,
+  CONFIRM_PHRASE,
+  handleDelete,
+}: DeleteAccountModalProps) {
+  return (
+    <div className="z-49 bg-black/50 backdrop-blur-sm text-white fixed top-1/2 left-1/2 -translate-x-1/3 -translate-y-1/2 flex flex-col items-center justify-center p-2 rounded-md h-[90%] w-fit">
+      <p className="p-3 text-lg">
+        Afin de supprimer votre compte veuillez indiquer votre mote de passe
+        ainsi qu'écrire la phrase ci-dessous:
+      </p>
+      <label htmlFor="password" className="mb-3 text-lg">
+        Mot de passe?
+      </label>
+      <input
+        id="password"
+        type="password"
+        name="password"
+        value={passwordInput}
+        onChange={(e) => setPasswordInput(e.target.value)}
+        className="rounded-sm p-2 text-black w-fit"
+      />
+      <label
+        htmlFor="delete"
+        className="mb-3 mt-6 text-lg"
+      >{`\"${CONFIRM_PHRASE}\"`}</label>
+      <input
+        id="delete"
+        type="text"
+        name="delete"
+        value={deleteInput}
+        onChange={(e) => setDeleteInput(e.target.value)}
+        className="rounded-sm p-2 text-black w-fit"
+      />
+      <div className="flex justify-between mt-2">
+        <button
+          className="p-4 m-2 bg-white text-red-500 w-fit rounded-md self-center"
+          onClick={() => (
+            setWantToDelete(false),
+            setDeleteInput(""),
+            setPasswordInput("")
+          )}
+        >
+          Annuler
+        </button>
+        <button
+          className="p-4 m-2 bg-green-400 text-white-500 w-fit rounded-md self-center"
+          onClick={handleDelete}
+        >
+          Confirmer
+        </button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Profil.tsx
+++ frontend/src/pages/Profil.tsx
@@ -7,10 +7,13 @@ import { useAuth } from "../auth/useAuth.ts";
 import * as api from "../api/api.ts";
 import { isValidMail } from "../utils/isValidMail.ts";
 
+import { Checkbox } from 'primereact/checkbox';
+        
 function Profil() {
   const { user } = useAuth();
 
   const [form, setForm] = useState({
+    id: user.id,
     pseudo: user.pseudo,
     email: user.email,
     newPassword: "",
@@ -25,6 +28,8 @@ function Profil() {
   const CONFIRM_PHRASE = `Je confirme vouloir supprimer mon compte: ${user.pseudo}`;
   const [wantToDelete, setWantToDelete] = useState<boolean>(false);
   const [passwordInput, setPasswordInput] = useState<string>("");
+  const [checked, setChecked] = useState(false);
+
 
   useEffect(() => {
     if (!feedback) return;
@@ -38,35 +43,79 @@ function Profil() {
 
   function handleSubmit() {
     if (!isUsernameValid) {
+      setForm((prev) => ({ ...prev, pseudo: user.pseudo }));
       setFeedback({ message: "Mauvais pseudo", type: "error" });
       return;
     }
+
     if (!resValidMail) {
+      setForm((prev) => ({ ...prev, email: user.email }));
       setFeedback({ message: "Mauvais email", type: "error" });
       return;
     }
-    if (isPasswordChanged && !isPasswordValid) {
+
+    if (isPasswordChange && !isPasswordValid) {
+      setForm((prev) => ({
+        ...prev,
+        newPassword: "",
+        confirmNewPassword: "",
+      }));
       setFeedback({ message: "Mot de passe invalide", type: "error" });
       return;
     }
+
     setFeedback({ message: "Chargement", type: "pending" });
     api
       .updateProfile(form)
-      .then(() =>
-        setFeedback({ message: "Changement effectué", type: "success" }),
-      )
+      .then(() => {
+        (setFeedback({ message: "Changement effectué", type: "success" }),
+          setTimeout(() => window.location.reload(), 2000));
+      })
       .catch((e) =>
         setFeedback({
           message: e instanceof Error ? e.message : String(e),
           type: "error",
         }),
+      )
+      .finally(() =>
+        setForm((prev) => ({
+          ...prev,
+          newPassword: "",
+          confirmNewPassword: "",
+        })),
       );
-
-    console.log(form);
   }
 
   function handleDelete() {
-    api.deleteAccount()
+    if (passwordInput.trim().length <= 0) {
+      setPasswordInput("");
+      setDeleteInput("");
+      setFeedback({
+        message: "Veuillez entrer votre mot de passe",
+        type: "error",
+      });
+      return;
+    }
+
+    if (deleteInput !== CONFIRM_PHRASE) {
+      setDeleteInput("");
+      setPasswordInput("");
+      setFeedback({
+        message: "Veuillez écrire la bonne phrase",
+        type: "error",
+      });
+      return;
+    }
+
+    const password = passwordInput;
+    api
+      .deleteAccount({ password })
+      .then(() =>
+        setFeedback({
+          message: "Votre compte a été supprimé.",
+          type: "success",
+        }),
+      )
       .catch((e) =>
         setFeedback({
           message: e instanceof Error ? e.message : String(e),
@@ -82,9 +131,9 @@ function Profil() {
       );
   }
 
-  const isPasswordChanged = form.newPassword.length > 0 && form.confirmNewPassword.length > 0;
-  const isPasswordValid = form.newPassword === form.confirmNewPassword && form.newPassword.length >= 8;
-  const isUsernameValid = form.pseudo.length >= 4;
+  const isPasswordChange = form.newPassword.length > 0 || form.confirmNewPassword.length > 0;
+  const isPasswordValid = form.newPassword === form.confirmNewPassword || form.newPassword.length >= 8;
+  const isUsernameValid = form.pseudo?.length >= 4;
   const resValidMail = isValidMail(form.email);
 
   return (
@@ -97,63 +146,36 @@ function Profil() {
           handleChange={handleChange}
           handleSubmit={handleSubmit}
         />
-        <DeleteAccountModal setWantToDelete={setWantToDelete} />
+        <div className="w-full flex justify-between items-center">
+          <div className="card flex justify-content-center">
+            <label htmlFor="2FA">Activer la double authentification (2FA)</label>
+            <Checkbox inputId='2FA' onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
+          </div>
+          <button
+            className="m-2 bg-red-600 text-white warning-hover hover:bg-white"
+            onClick={() => setWantToDelete(true)}
+          >
+            Supprimer le compte
+          </button>
+        </div>
         {feedback?.message && (
           <span
-            className={`fixed top-10 right-10 py-2 px-6 ${feedback.type === "success" ? "text-green-500" : feedback.type === "error" ? "text-red-500" : "text-white"} bg-black/50 backdrop-blur-md rounded-md`}
+            className={`z-50 fixed top-10 right-10 py-2 px-6 ${feedback.type === "success" ? "text-green-500" : feedback.type === "error" ? "text-red-500" : "text-white"} bg-black/50 backdrop-blur-md rounded-md`}
           >
             {feedback.message}
           </span>
         )}
       </div>
       {wantToDelete && (
-        <div className="z-50 bg-black/50 backdrop-blur-sm text-white fixed top-1/2 left-1/2 -translate-x-1/3 -translate-y-1/2 flex flex-col items-center justify-center p-2 rounded-md h-[90%] w-fit">
-          <p className="p-3 text-lg">
-            Afin de supprimer votre compte veuillez indiquer votre mote de passe
-            ainsi qu'écrire la phrase ci-dessous:
-          </p>
-          <label htmlFor="password" className="mb-3 text-lg">
-            Mot de passe?
-          </label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            value={passwordInput}
-            onChange={(e) => setPasswordInput(e.target.value)}
-            className="rounded-sm p-2 text-black w-fit"
-          />
-          <label
-            htmlFor="delete"
-            className="mb-3 mt-6 text-lg"
-          >{`\"${CONFIRM_PHRASE}\"`}</label>
-          <input
-            id="delete"
-            type="text"
-            name="delete"
-            value={deleteInput}
-            onChange={(e) => setDeleteInput(e.target.value)}
-            className="rounded-sm p-2 text-black w-fit"
-          />
-          <div className="flex justify-between mt-2">
-            <button
-              className="p-4 m-2 bg-white text-red-500 w-fit rounded-md self-center"
-              onClick={() => (
-                setWantToDelete(false),
-                setDeleteInput(""),
-                setPasswordInput("")
-              )}
-            >
-              Annuler
-            </button>
-            <button
-              className="p-4 m-2 bg-green-400 text-white-500 w-fit rounded-md self-center"
-              onClick={handleDelete}
-            >
-              Confirmer
-            </button>
-          </div>
-        </div>
+        <DeleteAccountModal
+          setWantToDelete={setWantToDelete}
+          deleteInput={deleteInput}
+          setDeleteInput={setDeleteInput}
+          passwordInput={passwordInput}
+          setPasswordInput={setPasswordInput}
+          CONFIRM_PHRASE={CONFIRM_PHRASE}
+          handleDelete={handleDelete}
+        />
       )}
     </div>
   );
