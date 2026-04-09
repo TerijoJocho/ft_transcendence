4a3f2870a7edeca030a25928268256adf2e589f2
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-04-09 15:35:15 +0200

    style: fixed linting errors

--- backend/src/auth/strategies/google-auth.strategy.ts
+++ backend/src/auth/strategies/google-auth.strategy.ts
@@ -9,31 +9,37 @@ import { eq } from 'drizzle-orm';
 @Injectable()
 export class GoogleAuthStrategy extends PassportStrategy(Strategy, 'google') {
   constructor(
-	private readonly signinService: SigninService,
-	private readonly utilsService: UtilsService
+    private readonly signinService: SigninService,
+    private readonly utilsService: UtilsService,
   ) {
-	super({
-	  clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
-	  clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-	  callbackURL: process.env.GOOGLE_AUTH_REDIRECT_URI,
-	  scope: ['email', 'profile'],
-	});
+    super({
+      clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_AUTH_REDIRECT_URI,
+      scope: ['email', 'profile'],
+    });
   }
 
   async validate(_accessToken: string, _refreshToken: string, profile: any) {
-	const { displayName, emails } = profile;
-	try {
-		const isExist = (await this.utilsService.findPlayersBy("and", undefined, eq(playerTable.mailAddress, emails[0].value))) as playerSelect[];
-		if (isExist.length === 0) {
-			const user = (await this.signinService.registerPlayers(
-				emails[0].value,
-				displayName
-			))[0];
-			return user;
-		}
-		return isExist[0];
-	} catch {
-		throw new ServiceUnavailableException("Cannot find or register new player with Google OAuth.");
-	}
+    const { displayName, emails } = profile;
+    try {
+      const isExist: playerSelect[] | { [x: string]: unknown }[] =
+        await this.utilsService.findPlayersBy(
+          'and',
+          undefined,
+          eq(playerTable.mailAddress, emails[0].value),
+        );
+      if (isExist && isExist.length === 0) {
+        const user: playerSelect | { [x: string]: unknown } = (
+          await this.signinService.registerPlayers(emails[0].value, displayName)
+        )[0];
+        return user;
+      }
+      return isExist[0];
+    } catch {
+      throw new ServiceUnavailableException(
+        'Cannot find or register new player with Google OAuth.',
+      );
+    }
   }
 }
