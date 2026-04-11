a389af25df583a567682faed253859d4d7d39bb5
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-04-11 14:02:17 +0200

    fix: added new rule trigger to default google guard behavior -> if access denied error when canceling oauth, redirection to localhost/login page

--- backend/src/auth/guards/passport-google-auth.guard.ts
+++ backend/src/auth/guards/passport-google-auth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import type { Request, Response } from 'express';
 
 @Injectable()
-export class PassportGoogleAuthGuard extends AuthGuard('google') {}
+export class PassportGoogleAuthGuard extends AuthGuard('google') {
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request = context.switchToHttp().getRequest<Request>();
+		const response = context.switchToHttp().getResponse<Response>();
+
+		if (request.query?.error === 'access_denied') {
+			response.redirect('https://localhost/login');
+			return false;
+		}
+
+		return (await super.canActivate(context)) as boolean;
+	}
+}
