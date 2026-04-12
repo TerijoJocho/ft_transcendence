714bcabc73816e3d3baa6905720848f6d205afb2
TerijoJocho <130741698+TerijoJocho@users.noreply.github.com>
2026-04-12 19:39:47 +0200

    fix: fix le merge du main et les pb de la db avec Aisha -> snapshot + journal + .sql

--- backend/drizzle/0007_sweet_pete_wisdom.sql
+++ backend/drizzle/0007_sweet_pete_wisdom.sql
@@ -1,3 +1,5 @@
-ALTER TABLE "friendship" ADD COLUMN "requesterId" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "friendship" ADD COLUMN "requesterId" integer;--> statement-breakpoint
+UPDATE "friendship" SET "requesterId" = "player1Id" WHERE "requesterId" IS NULL;--> statement-breakpoint
+ALTER TABLE "friendship" ALTER COLUMN "requesterId" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "friendship" ADD CONSTRAINT "friendship_requesterId_players_playerId_fk" FOREIGN KEY ("requesterId") REFERENCES "public"."players"("playerId") ON DELETE restrict ON UPDATE cascade;--> statement-breakpoint
 ALTER TABLE "friendship" ADD CONSTRAINT "requester_in_friendship" CHECK ("friendship"."requesterId" = "friendship"."player1Id" OR "friendship"."requesterId" = "friendship"."player2Id");
