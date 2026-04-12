fa25d25098b7af1df14ee6ab8324629acbe68b95
500 <kalvinsam31@gmail.com>
2026-04-12 12:40:41 +0200

    feat(2FA): mise en place methode 2FA

---
+++ backend/src/double_factor/double_factor.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { DoubleFactorService } from './double_factor.service';
+import { CreateDoubleFactorDto } from './dto/create-double_factor.dto';
+import { UpdateDoubleFactorDto } from './dto/UpdateDoubleFactorDto';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+import { PassportJwtGuard } from 'src/auth/guards/passport-jwt.guard';
+import { deleteDoubleFactorDto } from './dto/deleteDoubleFactorDto';
+
+@Controller('2FA')
+export class DoubleFactorController {
+  constructor(private readonly doubleFactorService: DoubleFactorService) {}
+
+  @Post('generate')
+  @UseGuards(PassportJwtGuard)
+  generate(@CurrentUser() user: CreateDoubleFactorDto) {
+    return this.doubleFactorService.setup2fa(user.userId);
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.doubleFactorService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.doubleFactorService.findOne(+id);
+  // }
+
+  @Patch('active')
+  @UseGuards(PassportJwtGuard)
+  update(
+    @CurrentUser() user: UpdateDoubleFactorDto,
+    @Body() data: { reply_code: string },
+  ) {
+    return this.doubleFactorService.active2fa(user, data.reply_code);
+  }
+
+  @Delete('delete')
+  @UseGuards(PassportJwtGuard)
+  remove(
+    @CurrentUser() user: { playerId: number },
+    @Body() data: deleteDoubleFactorDto,
+  ) {
+    return this.doubleFactorService.remove(user.playerId, data);
+  }
+}
