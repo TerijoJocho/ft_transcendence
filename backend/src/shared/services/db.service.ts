fab09c5e78b3fa6d55821c83bc2acd7d23e5dd75
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-04-13 20:32:07 +0200

    fix: linking schema to db upon db client instanciation for strong typing and avoiding any cascades and other linting issues later on in code

--- backend/src/shared/services/db.service.ts
+++ backend/src/shared/services/db.service.ts
@@ -1,18 +1,19 @@
 import { Pool } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import * as schema from '../db/schema';
 
 @Injectable()
 export class DatabaseService implements OnModuleInit, OnModuleDestroy {
   private pool: Pool;
-  public db: ReturnType<typeof drizzle>;
+  public db: NodePgDatabase<typeof schema>;
 
   onModuleInit() {
     const connectionString = process.env.POSTGRES_URL;
     if (!connectionString)
       throw new Error('POSTGRES_URL environment variable is not defined');
     this.pool = new Pool({ connectionString });
-    this.db = drizzle({ client: this.pool });
+    this.db = drizzle({ client: this.pool, schema });
   }
   async onModuleDestroy() {
     await this.pool.end();
