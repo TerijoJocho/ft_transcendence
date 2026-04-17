6122493d14d115d661f822d8360f8c86f39cf671
ayyshaaa <stierlen_aicha@hotmail.fr>
2026-04-17 20:10:02 +0200

    fix: added getAllPendingGamesData() func in utils service. It lists all pending games except pending game of player doing request if any

--- backend/src/shared/services/utils.func.service.ts
+++ backend/src/shared/services/utils.func.service.ts
@@ -698,4 +698,32 @@ export class UtilsService {
       points,
     };
   };
+
+  getAllPendingGamesData = async (playerId: number) => {
+    const query = this.Database.getDb()
+      .select({
+        gameId: gameTable.gameId,
+        gameMode: gameTable.gameMode,
+        creatorName: playerTable.playerName,
+        creatorColor: participationTable.playerColor,
+        gameCreatedAt: gameTable.gameCreatedAt,
+      })
+      .from(gameTable)
+      .innerJoin(
+        participationTable,
+        eq(participationTable.gameId, gameTable.gameId),
+      )
+      .innerJoin(
+        playerTable,
+        eq(participationTable.playerId, playerTable.playerId),
+      )
+      .where(
+        and(
+          eq(gameTable.gameStatus, 'PENDING'),
+          ne(participationTable.playerId, playerId),
+        ),
+      )
+      .orderBy(desc(gameTable.gameCreatedAt));
+    return query;
+  };
 }
